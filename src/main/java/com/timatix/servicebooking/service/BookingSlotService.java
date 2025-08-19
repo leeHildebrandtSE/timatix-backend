d8aaf4e18850b056bfe886ccdcf9f921bc762375
leeHildebrandtSE <leehildebrandt@rockmission.co.za>
2025-08-19 14:32:38 +0200

    Updated implementation.

---
+++ src/main/java/com/timatix/servicebooking/service/BookingSlotService.java
@@ -0,0 +1,33 @@
+package com.timatix.servicebooking.service;
+
+import com.timatix.servicebooking.model.BookingSlot;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public class BookingSlotService {
+    public List<BookingSlot> getAllSlots() {
+    }
+
+    public Optional<BookingSlot> getSlotById(Long id) {
+    }
+
+    public List<BookingSlot> getSlotsByDate(LocalDate date) {
+    }
+
+    public List<BookingSlot> getAvailableSlots() {
+    }
+
+    public List<BookingSlot> getAvailableSlotsByDate(LocalDate date) {
+    }
+
+    public BookingSlot createSlot(BookingSlot slot) {
+    }
+
+    public BookingSlot updateSlot(Long id, BookingSlot slotDetails) {
+    }
+
+    public void deleteSlot(Long id) {
+    }
+}
